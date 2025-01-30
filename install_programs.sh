67aef6d90848d0fca08e83e5df68fcc8346f7925
Angell6991 <miguel9196.guzman@gmail.com>
2025-01-30 18:26:25 -0500

    hypr

--- install_programs.sh
+++ install_programs.sh
@@ -5,11 +5,12 @@
 echo "Instalando programas..."
 
 # Programas esenciales
-sudo pacman -S  zathura dmenu neovim  kitty  ranger lxappearance font-manager python python-pip eog xournalpp unzip unrar zsh imagemagick  exa btop feh tlp bat git-delta dosfstools pavucontrol gnome-calendar gnome-clocks hyprlock 
+sudo pacman -S  zathura dmenu neovim  kitty  ranger lxappearance font-manager python python-pip eog xournalpp unzip unrar zsh imagemagick  exa btop feh  bat git-delta dosfstools pavucontrol gnome-calendar gnome-clocks hyprlock hypridle
 
 # config_tlp_gestor_de_energia
-sudo systemctl enable tlp 
-sudo systemctl start tlp 
+# sudo pacman -S tlp
+# sudo systemctl enable tlp 
+# sudo systemctl start tlp 
 
 # library_python
 sudo pacman -S tk python-matplotlib python-numpy
