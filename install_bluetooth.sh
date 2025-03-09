c79e2ebbaadb70c422d0c83a9a2e50457b3db9c8
Angell6991 <miguel9196.guzman@gmail.com>
2025-03-08 22:56:28 -0500

    config_001

--- install_bluetooth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S bluez-obex bluez bluez-utils blueman
-
-sudo systemctl enable bluetooth.service
-sudo systemctl start bluetooth.service
-sudo systemctl status bluetooth.service
-
-
