fe952a54de775f8b1790f1d37b0b743cf6b7a31b
Angell6991 <miguel9196.guzman@gmail.com>
2025-11-28 17:41:48 -0500

    pause and mute system

--- install_01.sh
+++ install_01.sh
@@ -14,7 +14,7 @@ echo    "Iniciando la instalando de programas"
 
 echo    "Programas esenciales:"
 
-pacman -S  zathura zathura-pdf-mupdf zathura-ps zathura-djvu zathura-cb neovim  kitty  yazi font-manager python python-pip eog xournalpp unzip unrar zsh imagemagick  exa btop  bat git-delta dosfstools pavucontrol gnome-calendar gnome-clocks hyprland hyprlock hypridle hyprpaper swww grim slurp mpv wf-recorder timeshift xclip wl-clipboard alsa-utils brightnessctl mako libnotify pamixer ffmpeg ly fastfetch firefox telegram-desktop gimp xdg-desktop-portal-hyprland gtk-layer-shell drawio-desktop man-db jq zbar pastel translate-shell reflector inkscape fontforge amberol yt-dlp fuzzel hyprpicker upower
+pacman -S  zathura zathura-pdf-mupdf zathura-ps zathura-djvu zathura-cb neovim  kitty  yazi font-manager python python-pip eog xournalpp unzip unrar zsh imagemagick  exa btop  bat git-delta dosfstools pavucontrol gnome-calendar gnome-clocks hyprland hyprlock hypridle hyprpaper swww grim slurp mpv wf-recorder timeshift xclip wl-clipboard alsa-utils brightnessctl mako libnotify pamixer ffmpeg ly fastfetch firefox telegram-desktop gimp xdg-desktop-portal-hyprland gtk-layer-shell drawio-desktop man-db jq zbar pastel translate-shell reflector inkscape fontforge amberol yt-dlp fuzzel hyprpicker upower wev playerctl
 
 pacman -S tgpt ufw
 
