1df16007d1211f792d6200506fb98b13bdd6c9d8
Angell6991 <miguel9196.guzman@gmail.com>
2026-01-25 14:10:48 -0500

    add git and base-level

--- 01_dependencies.sh
+++ 01_dependencies.sh
@@ -6,6 +6,7 @@
 
 pacman -S --noconfirm zathura zathura-pdf-mupdf zathura-ps zathura-djvu zathura-cb neovim  kitty  yazi font-manager python python-pip eog xournalpp unzip unrar zsh imagemagick  exa btop  bat git-delta dosfstools pavucontrol gnome-calendar gnome-clocks hyprland hyprlock hypridle hyprpaper swww grim slurp mpv wf-recorder timeshift xclip wl-clipboard alsa-utils brightnessctl mako libnotify pamixer ffmpeg ly fastfetch firefox telegram-desktop gimp xdg-desktop-portal-hyprland gtk-layer-shell drawio-desktop man-db jq zbar pastel translate-shell reflector inkscape fontforge amberol yt-dlp fuzzel hyprpicker upower wev playerctl
 
+pacman -S --noconfirm base-devel git 
 
 pacman -S --noconfirm gvfs-mtp mtpfs simple-mtpfs
 
