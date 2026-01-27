c522e15c58904c834952588b985071c62d25c4d4
Angell6991 <miguel9196.guzman@gmail.com>
2026-01-26 22:28:09 -0500

    add git and base-level

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo bash -c "./00_config_pacman.sh && ./01_dependencies.sh && ./02_init_services.sh && ./03_config_fonts.sh && ./04_set_time_and_bluetooth.sh"
+sudo bash 00_config_pacman.sh && bash 01_dependencies.sh && bash 02_init_services.sh && bash 03_config_fonts.sh && bash 04_set_time_and_bluetooth.sh
 
 bash 06_init_notify.sh
 bash 05_load_configurations.sh
