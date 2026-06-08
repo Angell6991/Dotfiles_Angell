f0a5ad1c7978e02c156e1f95e5478fb2d6fa5094
Angell6991 <miguel9196.guzman@gmail.com>
2026-06-08 16:07:09 -0500

    autentication

--- 01_dependencies.sh
+++ 01_dependencies.sh
@@ -8,7 +8,7 @@ pacman -S --noconfirm zathura zathura-pdf-mupdf zathura-ps zathura-djvu zathura-
 
 pacman -S --noconfirm base-devel git 
 
-pacman -S --noconfirm gvfs-mtp mtpfs simple-mtpfs cliphist 
+pacman -S --noconfirm gvfs-mtp mtpfs simple-mtpfs cliphist polkit-gnome
 
 pacman -S --noconfirm tgpt ufw udiskie mousai
 
